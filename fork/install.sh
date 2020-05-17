6ab28016a16c34c418015d8fa6f1e270b3e6e8f6
Ilaria Venturini <ilaria.venturini@accurat.it>
2020-05-17 11:24:05 +0200

    🍴 Update Fork script

---
+++ fork/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+current_date=`(date +%Y-%m-%d_%H%M%S)`
+
+# Set variables
+home=~/
+mos=~/mac-os-setup
+backup_dir=~/mac-os-setup-backup/fork/fork_$current_date
+
+#-------------------------------------
+# Install
+#-------------------------------------
+
+echo -e "\n› Check if Fork is installed..."
+
+# Check if Fork is installed
+if [! -d '/Applications/Fork.app' -a ! -d "$HOME/Applications/Fork.app"]
+then
+  echo -e '  Fork is not installed. Installing...'
+  brew install fork
+fi
+echo -e "  Fork already installed."
+echo -e "  ➜ Ok"
+
+#-------------------------------------
+# Settings
+#-------------------------------------
+
+echo -e "\n› Read the readme and set things manually."
